40262346a1fecb806eac3fab8b8a10649a1ab8d2
husonchen <1092441751@qq.com>
2017-07-02 00:06:49 +0800

    新增一个提示

---
+++ consumer/notify_luckmoney_fail.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import json
+import MySQLdb as mdb
+from util.memcache_util import Client
+import config
+import requests
+
+con = mdb.connect('127.0.0.1', 'xiaob', 'skdfjkasdf', 'xiaob')
+con.ping(True)
+cur = con.cursor()
+client = Client(config.prefix)
+url = 'https://api.weixin.qq.com/cgi-bin/message/custom/send?access_token='
+
+def handler(message):
+    try:
+        content = json.loads(message.body)
+        print content
+    except:
+        return True
+    access_token = client.get('access_token')
+    if not access_token:
+        cur.execute("SELECT v from server_config where k = 'access_token' and name_space='wx'")
+        data = cur.fetchone()
+        access_token = data[0]
+        client.set('access_token', access_token)
+
+    open_id = content['open_id']
+    data = '{"touser":"%s","msgtype":"text","text":{"content":"红包发放失败，由于您的用户状态异常，使用常用的活跃的微信号可避免这种情况，请联系淘宝客服索取红包。"}}'% open_id
+    requests.post(url,data=data)
+    return True
+
+
+
+
+
